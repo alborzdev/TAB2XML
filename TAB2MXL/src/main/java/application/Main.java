a621b0b9dd4ed194f8ccb309ababd36044b00522
Alborz Gharabaghi <alborzgithub@gmail.com>
2021-01-27 12:00:59 -0500

    test push 2

--- TAB2MXL/src/main/java/application/Main.java
+++ TAB2MXL/src/main/java/application/Main.java
@@ -29,6 +29,7 @@ public class Main extends Application {
 		controller.init(primaryStage);
 		Scene scene = new Scene(root);
 		System.out.println("Test");
+		System.out.println("Test2");
 		primaryStage.getIcons().add(new Image("https://icons-for-free.com/iconfiles/png/512/music+icon-1320184414432119131.png"));
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Tab 2 XML");
