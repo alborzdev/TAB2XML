3b80c0f3ba9465d5092264fcb31cc4f6c64e948c
isaiahL88 <isaiah.ib@outlook.com>
2021-02-08 16:42:06 -0500

    Added method to instantiate drum-kit

--- TAB2MXL/src/main/java/test/DrumReader.java
+++ TAB2MXL/src/main/java/test/DrumReader.java
@@ -1,18 +1,23 @@
 package test;
 
+import java.util.ArrayList;
+
 public class DrumReader {
 	private String [] measure;
 	private char [] column;
 	private int curr_col;
 	
 	//Each line in drum tab represents a part of the drumKit
-	private String [] drumKit = {"BaseDrum", "FloorTum", "SnareDrum", "MiddleTom", "HighTom", "HighHat", "Cymbol"};
+	private ArrayList<String> drumKit = new ArrayList<String>() ;
+	
 	//related to duration of note 
 	private int type;
 	
 	public DrumReader(String [] measure) {
 		this.measure = measure;
 		curr_col = 0;
+		initializeDrumKit();
+		
 	}
 	
 	/*
@@ -29,18 +34,42 @@ public class DrumReader {
 					type *= 2;
 				}
 				
-				
+				System.out.println(type + "note on " + this.drumKit.get(line));
 				
 			}
 		}
+	}
+	
+	/*
+	 * Add all instruments to drum kit
+	 */
+	private void initializeDrumKit() {
+		readColumn();
+		String instrument = "";
+		int col = this.curr_col;
+		
+		//go through initial line of instruments
+		for(int line = 0; line < this.column.length; line++) {
+			instrument = "";
+			
+			//go through all characters before measure line '|'
+			while(this.measure[line].charAt(col) != '|') {
+				instrument += this.measure[line].charAt(col);
+				col++;
+			}
+			
+			//adds instrument into drumkit
+			this.drumKit.add(instrument);
+		}
+		
 	}
 	
 	/* Takes one vertical line of notes from this.measure into this.column
 	 * Changed Derrui's guitar reader to work for drum tab
 	 */
 	private void readColumn() {
-		this.column = new char[this.drumKit.length];
-		for(int i=0; i<drumKit.length; i++) {
+		this.column = new char[this.measure.length];
+		for(int i=0; i<this.measure.length; i++) {
 			column[i] = measure[i].charAt(curr_col);
 		}
 		curr_col ++;
