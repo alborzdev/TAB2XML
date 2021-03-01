f9afdfaa14c4b903623b3ac0f7c7b07a37125f37
Alborz Gharabaghi <alborzgithub@gmail.com>
2021-03-01 13:12:31 -0500

    nothing

--- TAB2MXL/src/main/java/xmlClasses/ScorePartDrum.java
+++ TAB2MXL/src/main/java/xmlClasses/ScorePartDrum.java
@@ -12,30 +12,30 @@ public class ScorePartDrum extends Score_Part{
 	
 	public ScorePartDrum(String id, String partName, ArrayList<ScoreInstrument> sii) { 
 	    super(id, partName);
-	    sii.add(new ScoreInstrument("P1-I36", "Bass Drum 1"));
-	    sii.add(new ScoreInstrument("P1-I37", "Bass Drum 2"));
+	    si.add(new ScoreInstrument("P1-I36", "Bass Drum 1"));
+	    si.add(new ScoreInstrument("P1-I37", "Bass Drum 2"));
 	    sii.add(new ScoreInstrument("P1-I38", "Side Stick"));
-	    sii.add(new ScoreInstrument("P1-I39", "Snare"));
-	    sii.add(new ScoreInstrument("P1-I42", "Low Floor Tom"));
-	    sii.add(new ScoreInstrument("P1-I43", "Closed Hi-Hat"));
-	    sii.add(new ScoreInstrument("P1-I44", "High Floor Tom"));
-	    sii.add(new ScoreInstrument("P1-I45", "Pedal Hi-Hat"));
-	    sii.add(new ScoreInstrument("P1-I46", "Low Tom"));
-	    sii.add(new ScoreInstrument("P1-I47", "Open Hi-Hat"));
-	    sii.add(new ScoreInstrument("P1-I48", "Low-Mid Tom"));
-	    sii.add(new ScoreInstrument("P1-I49", "Hi-Mid Tom"));
-	    sii.add(new ScoreInstrument("P1-I50", "Crash Cymbal 1"));
-	    sii.add(new ScoreInstrument("P1-I51", "High Tom"));
-	    sii.add(new ScoreInstrument("P1-I52", "Ride Cymbal 1"));
-	    sii.add(new ScoreInstrument("P1-I53", "Chinese Cymbal"));
-	    sii.add(new ScoreInstrument("P1-I54", "Ride Bell"));
-	    sii.add(new ScoreInstrument("P1-I55", "Tambourine"));
-	    sii.add(new ScoreInstrument("P1-I56", "Splash Cymbal"));
-	    sii.add(new ScoreInstrument("P1-I57", "Cowbell"));
-	    sii.add(new ScoreInstrument("P1-I58", "Crash Cymbal 2"));
-	    sii.add(new ScoreInstrument("P1-I60", "Ride Cymbal 2"));
-	    sii.add(new ScoreInstrument("P1-I64", "Open Hi Conga"));
-	    sii.add(new ScoreInstrument("P1-I65", "Low Conga"));
+	    si.add(new ScoreInstrument("P1-I39", "Snare"));
+	    si.add(new ScoreInstrument("P1-I42", "Low Floor Tom"));
+	    si.add(new ScoreInstrument("P1-I43", "Closed Hi-Hat"));
+	    si.add(new ScoreInstrument("P1-I44", "High Floor Tom"));
+	    si.add(new ScoreInstrument("P1-I45", "Pedal Hi-Hat"));
+	    si.add(new ScoreInstrument("P1-I46", "Low Tom"));
+	    si.add(new ScoreInstrument("P1-I47", "Open Hi-Hat"));
+	    si.add(new ScoreInstrument("P1-I48", "Low-Mid Tom"));
+	    si.add(new ScoreInstrument("P1-I49", "Hi-Mid Tom"));
+	    si.add(new ScoreInstrument("P1-I50", "Crash Cymbal 1"));
+	    si.add(new ScoreInstrument("P1-I51", "High Tom"));
+	    si.add(new ScoreInstrument("P1-I52", "Ride Cymbal 1"));
+	    si.add(new ScoreInstrument("P1-I53", "Chinese Cymbal"));
+	    si.add(new ScoreInstrument("P1-I54", "Ride Bell"));
+	    si.add(new ScoreInstrument("P1-I55", "Tambourine"));
+	    si.add(new ScoreInstrument("P1-I56", "Splash Cymbal"));
+	    si.add(new ScoreInstrument("P1-I57", "Cowbell"));
+	    si.add(new ScoreInstrument("P1-I58", "Crash Cymbal 2"));
+	    si.add(new ScoreInstrument("P1-I60", "Ride Cymbal 2"));
+	    si.add(new ScoreInstrument("P1-I64", "Open Hi Conga"));
+	    si.add(new ScoreInstrument("P1-I65", "Low Conga"));
 	    //HARDCODED
 	    
 	} 
