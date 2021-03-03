06f33478fd8a5fc158c2ba5f719b28a16d017b68
Robert Aidan Mealey <mealey@my.yorku.ca>
2021-03-03 13:42:57 -0500

    working findNeedle

--- TAB2MXL/src/main/java/application/ErrorHandling.java
+++ TAB2MXL/src/main/java/application/ErrorHandling.java
@@ -2,14 +2,14 @@ package application;
 
 public class ErrorHandling {
 
-	public static void main(String[] args) {
-		String test1 = "qwertyuio";
-		String test2 = "rty";
-		
-		int[] ans = findNeedle(test1,test2);
-		System.out.println("Start at:"+ans[0]+"\nEnd at:"+ans[1]);
-		
-	}
+//	public static void main(String[] args) {
+//		String test1 = "helloasdhello";
+//		String test2 = "hello";
+//		
+//		int[] ans = findNeedle(test1,test2);
+//		System.out.println("Start at:"+ans[0]+"\nEnd at:"+ans[1]);
+//		
+//	}
 	
 	
 	
@@ -17,11 +17,17 @@ public class ErrorHandling {
 		//if no answer
 		if( !hay.contains(needle) ) return null;
 		//output
-		int[] indices = new int[2];
+		int[] indices = {0,0};
 		
-		for (int i = 0; i < hay.length; i++) {
-			if () {
-				
+		for(int i = 0; i < hay.length(); i++) {
+			if(hay.charAt(i)==needle.charAt(0)) {
+				for(int j = i; j < needle.length()+i; j++) {
+					//System.out.println(i+" "+hay.substring(i,j+1)+" "+j);
+					if( needle.equals( hay.substring(i,j+1) ) ) {
+						indices[0]=i;
+						indices[1]=j;
+					}
+				}
 			}
 		}
 		
