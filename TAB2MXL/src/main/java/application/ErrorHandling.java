34e6d5d5c2e5d718c111befa55c444e71182da47
Robert Aidan Mealey <mealey@my.yorku.ca>
2021-03-05 21:00:23 -0500

    Added error events to prevent

--- TAB2MXL/src/main/java/application/ErrorHandling.java
+++ TAB2MXL/src/main/java/application/ErrorHandling.java
@@ -1,23 +1,34 @@
 package application;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.jfoenix.controls.JFXTextArea;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import test.LineErrorException;
+
 public class ErrorHandling {
 
-//	public static void main(String[] args) {
-//		String test1 = "e|-------5-7-----7-|-8-----8-2-----2-|-0---------0-----|-----------------|\r\n"
-//				+ "B|-----5-----5-----|---5-------3-----|---1---1-----1---|-0-1-1-----------|\r\n"
-//				+ "G|---5---------5---|-----5-------2---|-----2---------2-|-0-2-2---2-------|\r\n"
-//				+ "D|-7-------6-------|-5-------5-------|-3---------------|-----------------|\r\n"
-//				+ "A|-----------------|------------|-----------------|-2-0-0---0---8-7-|\r\n"
-//				+ "E|-----------------|-----------------|-----------------|-----------------|";
-//		String test2 = "------------";
-//		
-//		//String[] str = test1.split("|", 0);
-//		
-//		int[] ans = findNeedle(test1,"|"+test2+"|");
-//		System.out.println("Start at:"+ans[0]+"\nEnd at:"+ans[1]);
-//		
-//	}
-	
+    public static boolean errorEvent(String Title, String Subtitle, Exception e) {
+    	
+    	
+    	AlertType type = AlertType.ERROR; 
+		Alert alert = new Alert(type, Title); 
+		alert.getDialogPane().setContentText(Subtitle); 
+		alert.showAndWait();
+		
+    	return true;
+    }
+    public static boolean errorEventHighlight(String Title, Exception e, JFXTextArea textarea, String problem) {
+    	
+    	int[] location = ErrorHandling.findNeedle(textarea.getText(), problem);
+    	textarea.selectRange(location[0], location[1]);
+    	
+		
+    	return errorEvent(Title, e.getMessage(), e);
+    }
 	
 	public static int[] findNeedle(String hay, String needle) {
 		//if no answer
