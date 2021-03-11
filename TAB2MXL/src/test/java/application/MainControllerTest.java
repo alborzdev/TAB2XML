f4b109fd31fcafb2c4f04a9d95df45fdbfa9ea0b
Lian Attily <lianattily@gmail.com>
2021-03-11 11:55:46 -0500

    TestFX dependency added to gradle.build

--- TAB2MXL/src/test/java/application/MainControllerTest.java
+++ TAB2MXL/src/test/java/application/MainControllerTest.java
@@ -4,12 +4,27 @@ import org.junit.jupiter.api.AfterEach;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxAssert;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.control.LabeledMatchers;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import xmlClasses.Attributes;
 import xmlClasses.PartWriter;
 
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,40 +32,60 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 	
-class MainControllerTest {
+class MainControllerTest extends ApplicationTest{
 	
+	Button button;
+	ComboBox<String> combobox;
+	 /* Just a shortcut to retrieve widgets in the GUI. */
+    public <T extends Node> T find(final String query) {
+        /** TestFX provides many operations to retrieve elements from the loaded GUI. */
+        return lookup(query).query();
+    }
+    
+//    @BeforeEach
+//    public void setUp() {
+//        /* Just retrieving the tested widgets from the GUI. */
+//
+//        button = find("#button");
+//        combobox = find("#combobox");
+//    }
+	/**
+     * Will be called with {@code @Before} semantics, i. e. before each test method.
+	 * @throws IOException 
+     */
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    	System.out.println(getClass().getResource("/fxml-files/Main.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml-files/Main.fxml"));
+		AnchorPane root = loader.load();
+		MainController controller = loader.getController();
+		controller.init(primaryStage);
 
-	private volatile boolean success = false;
+		Scene scene = new Scene(root,  1263, 517);
 
-	@BeforeEach
-	void setUp() {
-		//test=new MainController();
-	}
+		primaryStage.getIcons().add(new Image("https://icons-for-free.com/iconfiles/png/512/music+icon-1320184414432119131.png"));
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Tab 2 .xml");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+    }
 
-	@AfterEach
-	void clear() throws IOException {
-		
-	}
+    @Test
+    public void when_Uploadbutton_is_clicked_FileChooser() {
+        // when:
+        clickOn(".button");
 
-	@Test
-	void test1(){
-		assertThrows(FileNotFoundException.class,()-> tab2mxl.txtAnalyzing.analyze("somehwere"));
-	}
-	
-//	@Test
-//	void test2(){
-//		PartWriter p=new PartWriter();
-//		
-//		Assert.assertNotEquals(p.getPart(), null);
-//	}
-	
-	@Test
-	void test3() {
-		Attributes a=new Attributes(4,null,null,null);
-		assertEquals(a.getDivisions(),4);
-		assertEquals(a.getClef(),null);
-		assertEquals(a.getKey(),null);
-		assertEquals(a.getTime(),null);
-	}
+        // then:
+        FxAssert.verifyThat(".button", LabeledMatchers.hasText("Upload file"));
+        
+    }
+    
+    @Test
+    public void Attributes() {
+    	clickOn(".combobox");
+    	//clickOn(combobox).type(KeyCode.DOWN).type(KeyCode.ENTER);
+    	
+    	FxAssert.verifyThat("combobox", LabeledMatchers.hasText("Conversion Type"));
+    }
 
 }
