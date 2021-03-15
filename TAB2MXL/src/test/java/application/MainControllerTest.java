d461b7ef5323241a018c1e14a47465c95f8ca5f1
Lian Attily <lianattily@gmail.com>
2021-03-15 11:30:03 -0400

    MainControllerTest unit tests

--- TAB2MXL/src/test/java/application/MainControllerTest.java
+++ TAB2MXL/src/test/java/application/MainControllerTest.java
@@ -11,15 +11,18 @@ import org.testfx.matcher.control.LabeledMatchers;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import xmlClasses.Attributes;
 import xmlClasses.PartWriter;
@@ -28,6 +31,7 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -41,14 +45,7 @@ class MainControllerTest extends ApplicationTest{
         /** TestFX provides many operations to retrieve elements from the loaded GUI. */
         return lookup(query).query();
     }
-    
-//    @BeforeEach
-//    public void setUp() {
-//        /* Just retrieving the tested widgets from the GUI. */
-//
-//        button = find("#button");
-//        combobox = find("#combobox");
-//    }
+   
 	/**
      * Will be called with {@code @Before} semantics, i. e. before each test method.
 	 * @throws IOException 
@@ -81,11 +78,42 @@ class MainControllerTest extends ApplicationTest{
     }
     
     @Test
+    public void InstumentTypeheck() {
+    	ComboBox<String> combobox= find("#InstrumentType");
+    	clickOn("#InstrumentType");
+    	assertEquals(3, combobox.getItems().size());
+    }
+    @Test
+    public void ConversionTypeCheck() {
+    	ComboBox<String> combobox= find("#conversionType");
+    	clickOn("#conversionType");
+    	assertEquals(2, combobox.getItems().size());
+    }
+    
+    @Test
     public void Attributes() {
-    	clickOn(".combobox");
-    	//clickOn(combobox).type(KeyCode.DOWN).type(KeyCode.ENTER);
+    	TextField comp =find("#composer");
+    	clickOn(comp);
+    	comp.setText("Lian Attily");
+    	assertEquals("Lian Attily", comp.getText());
+    }
+   
+    @Test
+    public void Exit() {
+    	press(KeyCode.CONTROL).press(KeyCode.X).release(KeyCode.CONTROL).release(KeyCode.X);
     	
-    	FxAssert.verifyThat("combobox", LabeledMatchers.hasText("Conversion Type"));
+    }
+    
+    @Test
+    public void ReqAtt() throws InterruptedException {
+    	Button uploadbtn = find("#upload");
+    	Button export = find("#export");
+    	clickOn(uploadbtn);
+    	File inputFile = new File("/testTab.txt");
+    	System.out.println("PRINTING IN CONSOLE "+inputFile.getAbsolutePath());
+       // when(fileChooser.showOpenDialog(this.targetWindow())).thenReturn(inputFile);
+    	//clickOn(export);
+    //	FxAssert.verifyThat(export, LabeledMatchers.hasText("export"));
     }
 
 }
