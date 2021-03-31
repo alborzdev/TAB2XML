0a047794b8467adfddb999e950f3df3819be73ae
Lian Attily <lianattily@gmail.com>
2021-03-31 13:42:21 -0400

    broken debug

--- TAB2MXL/src/main/java/application/MainController.java
+++ TAB2MXL/src/main/java/application/MainController.java
@@ -18,15 +18,25 @@ import java.util.prefs.Preferences;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXToggleButton;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -39,7 +49,10 @@ public class MainController implements Initializable {
 	private Stage stage;
 	private File file;
 
-
+	@FXML
+	private JFXTextField from, to;
+	@FXML 
+	private JFXToggleButton toggle = new JFXToggleButton();
 	@FXML
 	private JFXTextArea ERRORStextarea;
 	@FXML 
@@ -62,10 +75,11 @@ public class MainController implements Initializable {
 	private JFXTextArea textarea;
 	
 	@FXML
-	private JFXTextArea measuresTEXTAREA;
+	private JFXTextArea measuresTEXTAREA = new JFXTextArea();
 
 	int []timesigs;
 	int size;
+	String tab;
 	Chain chain;
 	/**
 	 * This method allows Open/Upload button to select a .txt file and display it in text area
@@ -124,9 +138,16 @@ public class MainController implements Initializable {
 	 */
 	public void updateTimeSigsArray() throws Exception {
 		//NEW TIME SIGNATURE ARRAYS
-				List<String[]> TAB = new TabReaderV4( Chain.stringToFile( textarea.getText() ), 6).listMeasures();
+		File f = new File("TempD.txt");
+		 tab = tab2mxl.txtAnalyzing.analyze(f.toString());
+		System.out.println("TEXT AREA 148");
+		measuresTEXTAREA.appendText(tab);
+		System.out.println("measuresTEXTAREA "+tab);
+				List<String[]> TAB = new TabReaderV4( Chain.stringToFile( tab ), 6).listMeasures();
+				System.out.println("size = "+TAB.size());
 				for(int i=0;i<TAB.size();i++) {
 					String [] t=TAB.get(i);
+					System.out.println("printing t String[]");
 					for(int j=0;j<t.length;j++)	
 						System.out.println(t[j]);
 				}
@@ -136,10 +157,11 @@ public class MainController implements Initializable {
 					measures.getItems().add(i+1);
 					timesigs[i]=44;
 				}
+				
 	}
 	@FXML
 	public void changeMeasure(ActionEvent event) {
-		int m = measures.getSelectionModel().getSelectedItem();
+		int m = measures.getSelectionModel().getSelectedIndex();
 		String s = MeasureTimeSig.getSelectionModel().getSelectedItem();
 		System.out.println(s);
 		if(s.compareTo("3/4")==0)
@@ -275,7 +297,9 @@ public class MainController implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
+		//measuresTEXTAREA = new JFXTextArea();
+		//textarea = new JFXTextArea();
+		//measuresTEXTAREA.clear();
 		KeySig.getItems().add("C Major");
 		KeySig.getSelectionModel().select(0);
 		//		KeySig.getItems().add("G Major");
@@ -410,19 +434,19 @@ public class MainController implements Initializable {
 			alert.showAndWait().ifPresent(response -> {
 				if(response == YES) {
 					textarea.clear();
-					loader();
+					loader("database.txt","attributes.txt");
 				}
-				if(response==NO) { loader();}
+				if(response==NO) { loader("database.txt","attributes.txt");}
 			});;
 		}
-		else loader();
+		else loader("database.txt","attributes.txt");
 
 	}
 
-	public void loader() {
+	public void loader(String database, String attributes) {
 		BufferedReader br;
 		try {
-			br = new BufferedReader(new FileReader("database.txt"));
+			br = new BufferedReader(new FileReader(database));
 			String line;
 			try {
 				while ((line = br.readLine()) != null) {
@@ -447,7 +471,7 @@ public class MainController implements Initializable {
 		String text;
 		String [] parts;
 		try {
-			br = new BufferedReader(new FileReader("attributes.txt"));
+			br = new BufferedReader(new FileReader(attributes));
 			while((text = br.readLine()) != null) {
 				parts = text.split(",");
 				// now `parts` array will contain your data
@@ -476,11 +500,13 @@ public class MainController implements Initializable {
 
 	@FXML
 	public void printSelection(ActionEvent event) throws Exception {
-		List<String[]> TAB = new TabReaderV4( Chain.stringToFile( textarea.getText() ), 6).listMeasures();
+		System.out.println("print selection:");
+		List<String[]> TAB = new TabReaderV4( Chain.stringToFile( tab ), 6).listMeasures();
 		int i = measures.getSelectionModel().getSelectedIndex();
 			String [] t=TAB.get(i);
 			measuresTEXTAREA.clear();
 			for(int j=0;j<t.length;j++)	{
+				System.out.println(t[j]);
 				measuresTEXTAREA.appendText("|");
 				measuresTEXTAREA.appendText(t[j]);
 				measuresTEXTAREA.appendText("|\n");
@@ -490,12 +516,13 @@ public class MainController implements Initializable {
 	
 	@FXML
 	public void saveChanges(ActionEvent event) {
-		pref  =Preferences.userNodeForPackage(MainController.class); 
-		pref.put("Recent", textarea.getText());
-		pref.put("key", getKey());
-		pref.putInt("time", getTimeSig());
-		pref.put("type", getConversionType());
-		pref.put("instrument", getType());
+		if(textarea.getText().isBlank() || textarea.getText().isEmpty()) {
+			Alert conf = new Alert(AlertType.ERROR,  
+					"Could not save changes"); 
+			conf.setContentText("There is nothing to save");
+			conf.showAndWait(); 
+		}
+		else {
 		FileWriter fw;
 		try {
 			//pw.println(Fname+","+Lname+","+email+","+Password); 
@@ -538,5 +565,79 @@ public class MainController implements Initializable {
 			}
 		} 
 
+	}
+	}
+	private void save() {
+		FileWriter fw;
+		try {
+			{fw = new FileWriter("tempD.txt",false);
+
+			BufferedWriter bw = new BufferedWriter(fw); 
+			PrintWriter pw = new PrintWriter(bw); 
+			pw.println(textarea.getText());
+			pw.flush(); 
+			pw.close();}
+
+			//save attributes
+			fw = new FileWriter("tempA.txt",false);
+
+			BufferedWriter bw = new BufferedWriter(fw); 
+			PrintWriter pw = new PrintWriter(bw); 
+			pw.println(getKey()+","+getTimeSig()+","+getConversionType()+","+getType()+","+getComposer()+","+getTitle()+","+getLyricist());
+
+			pw.flush(); 
+			pw.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	@FXML
+	public void advancedOptions(ActionEvent event) throws Exception {
+		//save text area
+		save();
+		
+		Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml-files/advanced.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Advanced Options");
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+            // Hide this current window (if this is what you want)
+           // ((Node)(event.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("611");
+        textarea.clear();
+        loader("tempD.txt","tempA.txt");
+        System.out.println("613");
+        updateTimeSigsArray();
+	}
+	
+	@FXML
+	public void goBack(ActionEvent event) throws IOException {
+		Parent Scene2root = FXMLLoader.load(getClass().getResource("/fxml-files/Main.fxml"));
+		Scene AddInfoScene = new Scene(Scene2root);
+
+		//this gets scene information
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(AddInfoScene);
+		window.show();
+	}
+	ToggleGroup group = new ToggleGroup();
+	
+	@FXML
+	public void disableSingle(ActionEvent event) {
+		toggle.setToggleGroup(group);
+		Toggle temp = group.getSelectedToggle();
+		if(!temp.equals(null))
+			measures.setDisable(true);
+		else measures.setDisable(false);
+		System.out.println("disabling toggle function");
+		System.out.println(textarea.getText());
 	}
 }
