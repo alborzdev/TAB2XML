e48832ba616d624b812f2aa6f730845943e24b87
isaiahL88 <isaiah.ib@outlook.com>
2021-04-13 15:54:44 -0400

    New Tests, also cleaned up unnecessary code and added better comments

--- TAB2MXL/src/test/java/test/DrumReaderTest.java
+++ TAB2MXL/src/test/java/test/DrumReaderTest.java
@@ -15,6 +15,9 @@ import xmlClasses.AttributeWriter;
 
 class DrumReaderTest {
 
+	/* Test of the initializeDrumKit method, using the prof's given
+	 * example as the input
+	 */
 	@Test
 	void tabReaderDrumkitTest() {
 		String path = System.getProperty("user.dir") + "/drumTestTab.txt";
@@ -22,9 +25,9 @@ class DrumReaderTest {
 		tabReader.resetMeasure();// used to get drumkit
 		tabReader.readMeasure();
 		String[] measure = tabReader.getMeasure();
-//		System.out.println("TEST:" + measure[0]);
 		DrumReader drumReader = new DrumReader(measure);
 
+		//Expected instruments in the drum kit
 		ArrayList<String> testKit = new ArrayList<String>();
 		testKit.add("P1-I50");
 		testKit.add("P1-I43");
@@ -32,33 +35,19 @@ class DrumReaderTest {
 		testKit.add("P1-I48");
 		testKit.add("P1-I46");
 		testKit.add("P1-I36");
-		assertLinesMatch(testKit, drumReader.getDrumKit());	
-	}
-
-	@Test
-	void profExample() {
-		String path = System.getProperty("user.dir") + "/drumTestTab.txt";
-		try {
-			File fTab = null;
-			fTab=new File(path);
-			TabReaderV4 TRv4 = new TabReaderV4(fTab, 6, 0);
-			
-			
-		}catch(Exception e) {
-			
-		}
 		
+		assertLinesMatch(testKit, drumReader.getDrumKit());	
 	}
 	
 	/* Checks the DrumReader class correctly parses the first measure of the
 	 * prof's given example shown below
 	 * 
-	 *  CC|x---------------|--------x-------|*
-	 *  HH|--x-x-x-x-x-x-x-|----------------|*
-	 *  SD|----o-------o---|oooo------------|*
-	 *  HT|----------------|----oo----------|*
-	 *  MT|----------------|------oo--------|*
-	 *  BD|o-------o-------|o-------o-------|*/
+	 *  CC|x---------------|*
+	 *  HH|--x-x-x-x-x-x-x-|*
+	 *  SD|----o-------o---|*
+	 *  HT|----------------|*
+	 *  MT|----------------|*
+	 *  BD|o-------o-------|*/
 	@Test
 	void profExampleTest() {
 		String path = System.getProperty("user.dir") + "/drumTestTab.txt";
@@ -118,6 +107,111 @@ class DrumReaderTest {
 			
 		}catch (Exception e) {
 			
+		}
+	}
+	
+	/* Test to make sure Hi-Hat notes with an 'o' note-head are adjusted to
+	 * an open Hi-Hat
+	 *  CC|x---------------|*
+	 *  HH|--x-x-x-o-o-----|*
+	 *  SD|----------------|*
+	 *  HT|----------------|*
+	 *  MT|----------------|*
+	 *  BD|----------------|*/
+	@Test
+	void openHiHatTest() {
+		String path = System.getProperty("user.dir") + "/drumTestTab2.txt";
+		try {
+			//Loads Drum Tab into a string array
+			File fTab = null;
+			fTab = new File(path);
+			TabReaderV4 TRv4 = new TabReaderV4(fTab, 6, 0);
+			TRv4.readMeasure();
+			String [] tab = TRv4.getMeasure();
+			
+			DrumReader DR = new DrumReader(tab);// assumed 4/4
+			TRv4.readMeasure();
+			tab = TRv4.getMeasure();
+			DR.setMeasure(tab);
+			
+			ArrayList<String[]> actualNotes = new ArrayList<String[]>();
+			ArrayList<String[]> notes = new ArrayList<String[]>();
+
+			//Expected notes in the first measure
+			String [] note1 = {"A", "5", "2", "P1-I50", "1", "eighth", "x", "begin", "up"};
+			notes.add(note1);
+			String [] note3 = {"G", "5", "2", "P1-I43", "1", "eighth", "x", "continue", "up"};
+			notes.add(note3);
+			String [] note4 = {"G", "5", "2", "P1-I43", "1", "eighth", "x", "continue", "up"};
+			notes.add(note4);
+			String [] note5 = {"G", "5", "2", "P1-I43", "1", "eighth", "x", "end", "up"};
+			notes.add(note5);
+			String [] note6 = {"G", "5", "2", "P1-I47", "1", "eighth", "o", "begin", "up"};
+			notes.add(note6);
+			String [] note7 = {"G", "5", "2", "P1-I47", "1", "eighth", "o", "end", "up"};
+			notes.add(note7);
+			
+			//add all parsed notes in the first measure into a different array-list
+			while(DR.hasNext()) {
+				for(String [] s: DR.readNotes()) {
+					actualNotes.add(s);
+				}
+			}
+			
+			//check the values of each note
+			for(int i = 0; i < actualNotes.size(); i++) {
+				assertArrayEquals(notes.get(i), actualNotes.get(i));
+			}
+			
+		}catch (Exception e) {
+			
+		}
+	}
+	
+	/* Output Test of old row parsing for prof's given example*/
+	@Test 
+	void rowParsingTest(){
+		String path = System.getProperty("user.dir") + "/drumTestTab.txt";
+		try {
+			File fTab = null;
+			fTab=new File(path);
+
+			TabReaderV4 TRv4 = new TabReaderV4(fTab, 6, 0);
+		System.out.println("USING TABV4 NOW !!!!!");
+		
+		
+		TRv4.readMeasure();
+		DrumReader DR = new DrumReader(TRv4.getMeasure());//assumed 4/4
+		
+		
+		TRv4.readMeasure();
+		while(TRv4.hasNext()) {
+
+            DR.setMeasure(TRv4.getMeasure());
+			while(DR.hasNextRow()) {
+			System.out.println("Went Through to Read Row");
+			
+			for(String[] s:DR.readNoteRow()) {
+				if(s[0].equals("forward")) {
+					System.out.println("FORWARD " + s[1]  + "");
+				}else {
+					System.out.println("Step"+s[0]+
+							"Octave"+Integer.parseInt(s[1])+
+							"Duration"+Integer.parseInt(s[2])+
+							"Intrument"+s[3]+
+							"Voice"+s[4]+
+							"Type"+s[5]+
+							"NoteHead"+s[6]+
+							"Beam"+s[7]);
+				}
+
+				}
+			}
+			TRv4.readMeasure();	
+		}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
