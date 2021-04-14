cfa16d3da22255c0958ee77b95ce56e82e135cce
DerryL7889 <randomcount@outlook.com>
2021-04-13 23:53:05 -0400

    testing stuff

--- TAB2MXL/src/test/java/test/MeasureReaderV5Test.java
+++ TAB2MXL/src/test/java/test/MeasureReaderV5Test.java
@@ -21,18 +21,56 @@ class MeasureReaderV5Test {
 		try {
 			File file = new File(cfg.getAttr("test_path")+cfg.getAttr("test_file"));
 			TabReaderV5 tb = new TabReaderV5(file,6);
+			String output = "";
 			tb.readMeasure();
 			while(tb.hasNext()) {	
 				MeasureReaderV5 ms = new MeasureReaderV5(tb.getMeasure(),tb.getTuning(),4,4);
 				while(ms.hasNext()) {
 					ms.readNotes();
-					ms.getNotes();
+					for(String[] s:ms.getNotes()) {
+						for(int i = 0; i < s.length; i++) {
+							output += s[i] + ", ";
+						}
+						output += "\n";
+					}
 				}
 				tb.readMeasure();
-				//measure break
+				System.out.println(output);
 			}
 		}catch(Exception e) {
 			fail();
+		}
+	}
+	
+	void failingTabTestCase() {
+		
+		ConfigReader cfg = ConfigReader.getConfig();
+		System.out.println(cfg.getAttr("attr1"));
+		
+		try {
+			File file = new File(cfg.getAttr("test_path")+cfg.getAttr("bad_file"));
+			TabReaderV5 tb = new TabReaderV5(file,6);
+			String output = "";
+			tb.readMeasure();
+			while(tb.hasNext()) {	
+				MeasureReaderV5 ms = new MeasureReaderV5(tb.getMeasure(),tb.getTuning(),4,4);
+				while(ms.hasNext()) {
+					ms.readNotes();
+					for(String[] s:ms.getNotes()) {
+						for(int i = 0; i < s.length; i++) {
+							output += s[i] + ", ";
+						}
+						output += "\n";
+					}
+				}
+				tb.readMeasure();
+				System.out.println(output);
+			}
+			
+			fail();
+			
+		}catch(Exception e) {
+			
 		}
 	}
 
